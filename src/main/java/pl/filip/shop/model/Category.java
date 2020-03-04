226277afd2a68d5f7af44eb2acc3d118dc2a6b49
Filipx21 <filip.zajac2277@gmail.com>
2020-03-05 00:17:17 +0100

    category reformat

--- src/main/java/pl/filip/shop/model/Category.java
+++ src/main/java/pl/filip/shop/model/Category.java
@@ -1,6 +1,13 @@
 package pl.filip.shop.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.persistence.Column;
+
 import java.util.Collection;
 import java.util.Objects;
 
